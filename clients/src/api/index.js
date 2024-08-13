8b97c6e36abfa51a553961bd333f62141b6dbdac
Smudge <poms17226@gmail.com>
2024-08-12 21:10:53 -0400

    api fixing

--- clients/src/api/index.js
+++ clients/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://http://localhost:8080/api/",
+  baseURL: "http://localhost:8080/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
